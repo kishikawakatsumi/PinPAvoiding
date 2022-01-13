f0b1f6f040b17ec27ec74b1e90612610dc68d460
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2022-01-13 15:24:03 +0900

    Initial Commit

---
+++ PinPSafe/ViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var slider: UISlider!
+    let safeAreaVisualizeView = UIView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        safeAreaVisualizeView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(safeAreaVisualizeView)
+        safeAreaVisualizeView.backgroundColor = .systemBlue.withAlphaComponent(0.5)
+        NSLayoutConstraint.activate([
+            safeAreaVisualizeView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            safeAreaVisualizeView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            safeAreaVisualizeView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            safeAreaVisualizeView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let value = sender.value
+        additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: view.bounds.height * CGFloat(value), right: 0)
+    }
+}
